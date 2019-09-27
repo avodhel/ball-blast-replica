dee7b04104a7f678cbd110a5bc568611de4adcc4
avodhel <uyelik-eposta@hotmail.com>
2019-09-27 23:55:42 +0300

    level system - 1

---
+++ Blast_Ball_Replica_Umitcan/Assets/Scripts/LevelControl.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelControl : MonoBehaviour
+{
+    [Header("Level Bar")]
+    public Image lvlBarFilled;
+    public Text currentLvlText;
+    public Text nextLvlText;
+    [Header("Spawn")]
+    public GameObject spawnControl;
+    [Header("Meteors")]
+    public GameObject spawnedMeteor;
+    public GameObject splitMeteor;
+
+    int currentlvl = 1;
+    int nextLvl = 2;
+
+    void Start()
+    {
+        resetLvlValues();
+    }
+
+    void resetLvlValues()
+    {
+        lvlBarFilled.fillAmount = 0; //reset level bar
+        currentLvlText.text = currentlvl.ToString();
+        nextLvlText.text = nextLvl.ToString();
+    }
+}
