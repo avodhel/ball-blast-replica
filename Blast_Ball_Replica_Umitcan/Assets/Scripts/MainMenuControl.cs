65f6999851b0243699dc0f7793ff0334874d04b7
avodhel <uyelik-eposta@hotmail.com>
2019-09-28 16:30:50 +0300

    main menu

---
+++ Blast_Ball_Replica_Umitcan/Assets/Scripts/MainMenuControl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuControl : MonoBehaviour
+{
+    public void startGame()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
